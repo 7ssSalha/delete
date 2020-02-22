f716fdb5f3ebde6d33a6dbd1bc6eb9d2ff75fbb9
7ssSalha <husseinsoheil1999@hotmail.com>
2020-02-22 20:52:37 +0200

    1

---
+++ src/testcases/test1.java
@@ -0,0 +1,18 @@
+package testcases;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class test1 {
+
+	@Test
+	public void t1() throws Exception {
+		assertEquals(add(5,4),9);
+	}
+
+	public int add(int i, int j) {
+		// TODO Auto-generated method stub
+		return 9;
+	}
+
+}
